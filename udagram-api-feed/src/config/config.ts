986eea3c22d348841cd0e308b3d057672814e083
RawanAhmed <rawanahmed1251992@gmail.com>
2021-03-25 18:24:01 +0200

    commit correct credentials

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = {
-  'username': 'chirperadmin',
-  'password': 'postgres',
-  'database': 'chirperdb',
-  'host': 'wyr.cfjeibujcbd0.us-east-1.rds.amazonaws.com',
+  'username': 'postgres',
+  'password': 'root1234',
+  'database': 'udacitydev',
+  'host': 'udacitydev.c8jkuqlx5fai.us-east-2.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': 'us-east-1',
+  'aws_region': 'us-east-2',
   'aws_profile': 'default',
-  'aws_media_bucket': 'test-376262390406-dev',
+  'aws_media_bucket': 'rawan-786630331230-dev',
   'url': 'http://localhost:8100',
   'jwt': {
     'secret': 'hello',
